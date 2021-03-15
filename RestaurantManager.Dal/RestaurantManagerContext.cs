07b19966d0b8de905267e706c9c6d1dae7a3fd92
Paweł Wołkowski <pawel.wolkowski@ialbatros.com>
2021-03-15 16:06:53 +0100

    init

---
+++ RestaurantManager.Dal/RestaurantManagerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantManager.Dal
+{
+    public class RestaurantManagerContext : DbContext
+    {
+        public RestaurantManagerContext (DbContextOptions<RestaurantManagerContext> options)
+            : base(options)
+        {
+        }
+        
+    }
+}
